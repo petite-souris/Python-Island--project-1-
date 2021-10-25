74b54830d31b4b903449c1b7e92990c9476188ee
petite-souris <cecilia.conquet@gmail.com>
2021-10-25 11:40:51 +0200

    Ask for the rules in the game or not.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import time
 # Lecture of other files
 with open("Textes/bienvenue.txt", "r", encoding="utf-8") as bienvenue:
     ligneB = bienvenue.readlines()
+with open("Textes/regles.txt", "r", encoding="utf-8") as regles:
+    ligneR = regles.readlines()
 
 # starting
 def main():
@@ -20,7 +22,27 @@ def main():
     reply = input()
     if reply != 'o' or reply != 'oui' or reply != 'O' or reply != 'OUI':
         input("Quel est ton nom cher aventurier ? \n")
+
+        # rules of the game
+        print("Avant de commencer le jeu, veux-tu d'abord lire les règles ?")
+        reply = "o"
+        reply = input()
+        if reply == "o":
+            for ligne in ligneR:
+                print(ligne)
+                time.sleep(1.0)
+            print("\nMaintenant que les règles ont été énumérées, voici le début de l'histoire !\n")
+            for ligne in ligneS:
+                print(ligne)
+                time.sleep(1.0)
+        else:
+            # scenario
+            for ligne in ligneS:
+                print(ligne)
+                time.sleep(1.0)
+
     else:
+        # load a save
         pass
 
 if __name__ == "__main__":
