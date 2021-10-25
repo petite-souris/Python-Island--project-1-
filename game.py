6b0def03802544071a1213f7cf20d55bd858f483
petite-souris <cecilia.conquet@gmail.com>
2021-10-25 14:56:49 +0200

    Imports of all cases and files needed.

--- game.py
+++ game.py
@@ -1,8 +1,18 @@
 # code utf-8
 
 # imports
-from Map import mapmain
+from Map.mapmain import mapmain
+from MysteriousNumber.game import *
+from FizzBuzz.game import *
+from CaesarCode.main import *
+from Player.player import *
+from inventory import *
+# from Menu import *
+# from FinalDoor.main import *
+
 import time
+import os
+import json
 
 # other files
 with open("Texts/starting.txt", "r", encoding="utf-8") as starting:
@@ -15,6 +25,8 @@ def game():
         time.sleep(1.0)
         
     print(mapmain)
+
+
 
 if __name__ == '__main__':
     game()
