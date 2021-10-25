ef3f6a101b2b515bef633c18c2ef204a6fe4937a
petite-souris <cecilia.conquet@gmail.com>
2021-10-25 11:31:20 +0200

    New player

--- main.py
+++ main.py
@@ -15,5 +15,13 @@ def main():
         print(ligne)
         time.sleep(1.0)
 
+    # new player
+    print("Es-tu un nouveau joueur ? \n")
+    reply = input()
+    if reply != 'o' or reply != 'oui' or reply != 'O' or reply != 'OUI':
+        input("Quel est ton nom cher aventurier ? \n")
+    else:
+        pass
+
 if __name__ == "__main__":
     main()
